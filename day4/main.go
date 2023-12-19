948daccd60dd1f2e196956d192e65575712c97ed
Siddhartha Khuntia <siddharthakhuntia@gmail.com>
2023-12-20 02:46:30 +0530

    day4 solved
    Signed-off-by: Siddhartha Khuntia <siddharthakhuntia@gmail.com>

---
+++ day4/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+type void struct{}
+
+var member void
+
+func solve() {
+	input, err := os.ReadFile("input2.txt")
+
+	if err != nil {
+		fmt.Println("File reading error", err)
+		return
+	}
+
+	ans := 0
+	stringInput := string(input)
+
+	rows := strings.Split(stringInput, "\n")
+	var rowSet []string
+
+	for _, row := range rows {
+		temp := strings.Split(row, ": ")
+		numbers := strings.TrimSpace(temp[1])
+		rowSet = append(rowSet, numbers)
+
+	}
+
+	prefixSum := make([]int, len(rowSet))
+
+	for i := 0; i < len(rowSet); i++ {
+		prefixSum[i] = 1
+	}
+
+	for cardIdx, row := range rowSet {
+		numArr := strings.Split(row, " | ")
+
+		set := make(map[string]void)
+
+		winningNums := strings.Split(numArr[0], " ")
+
+		for _, num := range winningNums {
+			// formattedNum := strings.ReplaceAll(num, "\"", "")
+			num = strings.TrimSuffix(num, "\n")
+			if num == "" {
+				continue
+			}
+			set[num] = member
+		}
+
+		numberWeHave := strings.Split(numArr[1], " ")
+		count := 0
+		for _, num := range numberWeHave {
+			num = strings.TrimSuffix(num, "\n")
+			// formattedNum := strings.ReplaceAll(num, "\"", "")
+			if num == "" {
+				continue
+			}
+			if _, ok := set[num]; ok {
+				count++
+			}
+		}
+		for i := 0; i < count; i++ {
+			prefixSum[cardIdx+1+i] += prefixSum[cardIdx]
+		}
+
+	}
+
+	for _, num := range prefixSum {
+		ans += num
+	}
+
+	fmt.Printf("Ans: %v\n", ans)
+
+}
+
+func main() {
+	solve()
+}
